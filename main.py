94a1bafd056a86ff24400dfe594ea9abc4e199fa
Ole4kaxxvii <Ole4ka.abr@gmail.com>
2026-05-24 06:39:32 +0300

    Добавил скрипт для сохранения DataFrame в .dbf

--- main.py
+++ main.py
@@ -1,51 +1,35 @@
+from dbfread import DBF
 import pandas as pd
 
-# Таблица студентов
-students = pd.DataFrame({
-    'StudentID': [1, 2, 3, 4, 5],
-    'LastName': ['Иванов', 'Петрова', 'Сидоров', 'Кузнецова', 'Морозов'],
-    'FirstName': ['Петр', 'Анна', 'Алексей', 'Мария', 'Дмитрий'],
-    'MiddleName': ['Александрович', 'Борисовна', 'Викторович', 'Игоревна', 'Павлович'],
-    'Faculty': [
-        'Факультет информатики',
-        'Факультет математики',
-        'Факультет физики',
-        'Факультет химии',
-        'Факультет биологии'
-    ],
-    'Course': [3, 2, 4, 1, 3]
+# Импортируем данные из файла genres.dbf
+try:
+    dbf = DBF('genres.dbf')
+    genres = [record for record in dbf]
+    genres_df = pd.DataFrame(genres)
+    print("Данные из genres.dbf успешно загружены:")
+    print(genres_df)
+except FileNotFoundError:
+    print("Файл genres.dbf не найден. Создаем пример DataFrame.")
+
+    # Пример исправленного DataFrame, если файла нет
+    genres_df = pd.DataFrame({
+        'GenreID': [1, 2, 3],
+        'GenreName': ['Фантастика', 'Драма', 'Комедия']
+    })
+
+# Выводим пример DataFrame
+print("\nПример DataFrame genres:")
+print(genres_df)
+
+# Создаем дополнительные DataFrame для примера
+movies = pd.DataFrame({
+    'MovieID': [101, 102, 103],
+    'Title': ['Фильм 1', 'Фильм 2', 'Фильм 3'],
+    'GenreID': [1, 2, 3]
 })
 
-# Таблица факультетов
-faculties = pd.DataFrame({
-    'FacultyID': [1, 2, 3, 4, 5],
-    'FacultyName': [
-        'Факультет информатики',
-        'Факультет математики',
-        'Факультет физики',
-        'Факультет химии',
-        'Факультет биологии'
-    ]
-})
+# Можно объединить таблицы или выполнить другие операции
+movies_with_genres = pd.merge(movies, genres_df, on='GenreID', how='left')
 
-# Таблица групп
-groups = pd.DataFrame({
-    'GroupID': [1, 2, 3, 4, 5],
-    'GroupName': ['Группа 1', 'Группа 2', 'Группа 3', 'Группа 4', 'Группа 5']
-})
-
-# Связь студентов с группами
-student_group = pd.DataFrame({
-    'StudentID': [1, 2, 3, 4, 5],
-    'GroupID': [1, 2, 3, 4, 5]
-})
-
-# Вывод таблиц (по желанию)
-print("Таблица студентов:")
-print(students)
-print("\nТаблица факультетов:")
-print(faculties)
-print("\nТаблица групп:")
-print(groups)
-print("\nСвязь студентов с группами:")
-print(student_group)
+print("\nТаблица фильмов с жанрами:")
+print(movies_with_genres)
